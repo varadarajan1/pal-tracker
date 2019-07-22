7bdd229972a3342243fd27f14937e2bd582f9140
Varadarajan V <vvaradu@gmail.com>
2019-07-22 16:51:50 +0530

    Add Springboot controller

---
+++ src/main/java/io/pivotal/pal/tracker/PalApplication.java
@@ -0,0 +1,11 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalApplication{
+    public static void main(String[] args) {
+        SpringApplication.run(PalApplication.class,args);
+    }
+}
